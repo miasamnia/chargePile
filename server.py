4a419ef2e347f272c1ec81eccf33d77ca013f2ad
miasamnia <1937148482@qq.com>
2023-06-06 23:47:08 +0800

    1

--- server.py
+++ server.py
@@ -7,9 +7,8 @@ IP = '0.0.0.0'
 PORT = 56789
 BUFLEN = 512
 
-
-log = open('log/'+time.asctime().replace(':','_')+'.txt', 'w')
-#cd = open('cd.txt','w')
+log = open('log/' + time.asctime().replace(':', '_') + '.txt', 'w')
+# cd = open('cd.txt','w')
 room = ''
 beHost = True  # True表示他该开房，否则加入
 
@@ -20,28 +19,32 @@ listenSocket.listen(5)
 
 def check_user(name, pwd):
     if name in os.listdir('data/users'):
-        f = open('data/users/'+name+'/pwd','r')
-        p = f.readline()
-        f.close()
-        if pwd == p:
-            return True
-    return False
-def check_admin(name, pwd):
-    if name in os.listdir('data/admins'):
-        f = open('data/admins/'+name+'/pwd','r')
+        f = open('data/users/' + name + '/pwd', 'r')
         p = f.readline()
         f.close()
         if pwd == p:
             return True
     return False
 
-def checkroom(room):#8位字母数字组合
-    if room.__len__()==8:
+
+def check_admin(name, pwd):
+    if name in os.listdir('data/admins'):
+        f = open('data/admins/' + name + '/pwd', 'r')
+        p = f.readline()
+        f.close()
+        if pwd == p:
+            return True
+    return False
+
+
+def checkroom(room):  # 8位字母数字组合
+    if room.__len__() == 8:
         return room.isalnum()
     else:
         return False;
 
-#def checkCD(name):
+
+# def checkCD(name):
 
 while True:
     try:
@@ -51,10 +54,16 @@ while True:
         info = received.decode()
         log.writelines(info + '  from' + f'{addr}\n')
         log.flush()
-        name,pwd,act=json.loads(received)
+        request = json.loads(info)
+        # name = request['username']
+        # pwd = request['password']
+        # act = request['request']
+        act=request[0]
+        name=request[1]
+        pwd=request[2]
         print(received)
-        #name, pwd, act = info.split('|')
-        if(act=='register'):
+        # name, pwd, act = info.split('|')
+        if act == 'register':
             if name in os.listdir('data/users'):
                 dataSocket.send('account already exist!'.encode())
                 log.writelines('account already exist:{name}\n')
@@ -65,21 +74,21 @@ while True:
                 f.writelines(pwd)
                 log.writelines('register succeed:{name}\n')
                 log.flush()
-        elif(act=='login'):
+                dataSocket.send('register succeed!'.encode())
+        elif act == 'login':
             legal = check_user(name, pwd)
             if not legal:
                 dataSocket.send('password or account wrong!'.encode())
                 log.writelines('login failed\n')
                 log.flush()
             else:
+                dataSocket.send('login succeed!'.encode())
                 log.writelines('login succeed:{name}\n')
                 log.flush()
-        elif(act=='admin'):
-            legal=check_admin()
+        elif act == 'admin':
+            legal = check_admin()
 
     except Exception as e:
         log.writelines(f'ERROR: {e}\n')
         log.flush()
         continue
-
-
--- user_client.py
+++ user_client.py
@@ -2,7 +2,7 @@ import sys
 from  socket import *
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
 import json
 
 
@@ -18,53 +18,64 @@ class MainWin(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ui.ui", self)
+        self.in_pwd.setEchoMode(QLineEdit.Password)
+        self.in_act.setPlaceholderText("不区分大小写")
         self.show_info.setAlignment(Qt.AlignHCenter)
-        self.regesterButton.clicked.connect(self.regester)
         self.loginButton.clicked.connect(self.login)
+        self.registerButton.clicked.connect(self.register)
 
-    def regester(self):
+    def register(self):
         name = self.in_act.text().lower()
         pwd = self.in_pwd.text()
-        dataSocket = socket(AF_INET, SOCK_STREAM)
-        dataSocket.connect((IP, SERVER_PORT))
-        request = {
-            'username': name,
-            'password': pwd,
-            'request': 'register',
-        }
-        data=json.dumps((request))
-        #tosend = name + '|' + pwd + '|1'
-        dataSocket.send(data.encode())
-
-        received = dataSocket.recv(BUFLEN).decode()
-        rec=json.loads(received)
-        if rec['status'] == 'account already exist!':
-            self.show_info.setText(received)
+        if len(name) == 0 or len(pwd) == 0:
+            self.show_info.setText('不允许为空')
             self.show_info.setAlignment(Qt.AlignHCenter)
         else:
-            self.show_info.setText("register succeed!")
+            dataSocket = socket(AF_INET, SOCK_STREAM)
+            dataSocket.connect((IP, SERVER_PORT))
+            # request = {
+            #     'username': name,
+            #     'password': pwd,
+            #     'request': 'register',
+            # }
+            request=['register',name,pwd]
+            data=json.dumps(request)
+            #tosend = name + '|' + pwd + '|1'
+            dataSocket.send(data.encode())
+
+            received = dataSocket.recv(BUFLEN).decode()
+            rec=json.loads(received)
+            if rec['status'] == 'account already exist!':
+                self.show_info.setText(received)
+                self.show_info.setAlignment(Qt.AlignHCenter)
+            else:
+                self.show_info.setText("register succeed!")
 
     def login(self):
         name = self.in_act.text().lower()
         pwd = self.in_pwd.text()
-        dataSocket = socket(AF_INET, SOCK_STREAM)
-        dataSocket.connect((IP, SERVER_PORT))
-        request = {
-            'username': name,
-            'password': pwd,
-            'request': 'login',
-        }
-        data = json.dumps((request))
-        # tosend = name + '|' + pwd + '|1'
-        dataSocket.send(data.encode())
-
-        received = dataSocket.recv(BUFLEN).decode()
-        rec = json.loads(received)
-        if rec['status'] == 'password or account wrong!':
-            self.show_info.setText(received)
+        if len(name) == 0 or len(pwd) == 0:
+            self.show_info.setText('不允许为空')
             self.show_info.setAlignment(Qt.AlignHCenter)
         else:
-            self.show_info.setText("login succeed!")
+            dataSocket = socket(AF_INET, SOCK_STREAM)
+            dataSocket.connect((IP, SERVER_PORT))
+            request = {
+                'username': name,
+                'password': pwd,
+                'request': 'login',
+            }
+            data = json.dumps(request)
+            # tosend = name + '|' + pwd + '|1'
+            dataSocket.send(data.encode())
+
+            received = dataSocket.recv(BUFLEN).decode()
+            rec = json.loads(received)
+            if rec['status'] == 'password or account wrong!':
+                self.show_info.setText(received)
+                self.show_info.setAlignment(Qt.AlignHCenter)
+            else:
+                self.show_info.setText("login succeed!")
 
 
 if __name__ == "__main__":
