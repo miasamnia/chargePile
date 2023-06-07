fdbf3bcb2703a821a718c15c7f6964d98472e93f
miasamnia <1937148482@qq.com>
2023-06-07 17:13:24 +0800

    前端通信更新： request=['__login',name,pwd,0]             received=datasend(request)             rec = json.loads(received)

--- user_client.py
+++ user_client.py
@@ -1,24 +1,23 @@
 import sys
-from  socket import *
+from socket import *
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QWidget
 import json
 
-
-#DOMAIN = 'cher.中国'
-#IP = getaddrinfo(DOMAIN, None)[0][4][0]
-#IP = '101.42.228.137'
+# IP = '101.42.228.137'
 IP = '127.0.0.1'
 SERVER_PORT = 56789
 BUFLEN = 512
-CDTIME = 60#匹配成功一次的cd
+CDTIME = 60  # 匹配成功一次的cd
+
 
 def datasend(data):
     dataSocket = socket(AF_INET, SOCK_STREAM)
     dataSocket.connect((IP, SERVER_PORT))
     dataSocket.send(json.dumps(data).encode())
     return dataSocket.recv(BUFLEN).decode()
+
 
 class MainWin(QMainWindow):
     def __init__(self):
@@ -37,25 +36,15 @@ class MainWin(QMainWindow):
             self.show_info.setText('不允许为空')
             self.show_info.setAlignment(Qt.AlignHCenter)
         else:
-            # dataSocket = socket(AF_INET, SOCK_STREAM)
-            # dataSocket.connect((IP, SERVER_PORT))
-            # request = {
-            #     'username': name,
-            #     'password': pwd,
-            #     'request': 'register',
-            # }
-            request=['__register',name,pwd,0]
-            data=json.dumps(request)
-            #tosend = name + '|' + pwd + '|1'
-            # dataSocket.send(data.encode())
-            # received = dataSocket.recv(BUFLEN).decode()
+            request = ['__register', name, pwd, 0]
+            data = json.dumps(request)
             received = datasend(data)
-            rec=json.loads(received)
+            rec = json.loads(received)
             if rec['status'] == 'account already exist!':
                 self.show_info.setText(received)
                 self.show_info.setAlignment(Qt.AlignHCenter)
             else:
-                self.show_info.setText("register succeed!")
+                self.show_info.setText("register succeed!\nyou may login now")
 
     def login(self):
         name = self.in_act.text().lower()
@@ -64,19 +53,8 @@ class MainWin(QMainWindow):
             self.show_info.setText('不允许为空')
             self.show_info.setAlignment(Qt.AlignHCenter)
         else:
-            # dataSocket = socket(AF_INET, SOCK_STREAM)
-            # dataSocket.connect((IP, SERVER_PORT))
-            # request = {
-            #     'username': name,
-            #     'password': pwd,
-            #     'request': 'login',
-            # }            # data = json.dumps(request)
-            #             # # tosend = name + '|' + pwd + '|1'
-            #             # dataSocket.send(data.encode())
-            #
-            #             # received = dataSocket.recv(BUFLEN).decode()
-            request=['__login',name,pwd,0]
-            received=datasend(request)
+            request = ['__login', name, pwd, 0]
+            received = datasend(request)
             rec = json.loads(received)
             if rec[1] == 0:
                 self.show_info.setText(received)
@@ -85,11 +63,24 @@ class MainWin(QMainWindow):
                 self.show_info.setText("login succeed!")
                 self.loginButton.setVisible(False)
                 self.registerButton.setVisible(False)
+                subwin.show()
+                mainwin.setVisible(False)
 
+class SubWin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('subwindow.ui',self)
+        self.chargemodeButton.setVisible(False)
+        self.chargechangeButton.setVisible(False)
+        self.stopchargingButton.setVisible(False)
+        self.chargeamountline.setVisible(False)
+        self.chargeamounttext.setVisible(False)
+        self.modeselectiontext.setVisible(False)
+        self.modeselectioncombo.setVisible(False)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainwin = MainWin()
     mainwin.show()
-
+    subwin=SubWin()
     sys.exit(app.exec_())
