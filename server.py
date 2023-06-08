8c496bd04ff2f95170e142aacbbe01d2ce887b78
miasamnia <1937148482@qq.com>
2023-06-08 22:27:49 +0800

    请求充电、改变充电模式

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import time
 from socket import *
 import json
 import sqlite3
+import threading
+import queue
 
 IP = '0.0.0.0'
 PORT = 56789
@@ -16,6 +18,23 @@ beHost = True  # True表示他该开房，否则加入
 listenSocket = socket(AF_INET, SOCK_STREAM)
 listenSocket.bind((IP, PORT))
 listenSocket.listen(5)
+
+waiting_queue = queue.Queue(maxsize=6)  # 等待区队列
+# fast_pile = [True, True]  # 快充桩是否忙
+# slow_pile = [False, False, False]  # 慢充是否忙
+fast_pile1={}
+fast_pile2={}
+slow_pile1={}
+slow_pile2={}
+slow_pile3={}
+fast_waiting=[True,True]#充电区的等候位置
+slow_waiting=[True,True]
+f=0#等候区快充编号
+s=0#等候区慢充编号
+time = 5 * 60 + 55
+billid=0
+
+
 
 
 def check_user(name, pwd):
@@ -75,11 +94,79 @@ def login(name, pwd):
         log.flush()
 
 
-def chargereq(name):
-    datasend(['__SubmitRequestReturn', 1, {'Billid': 1, 'USERID': '1', 'CreateTime':
-        1684540800, 'chargeMode': 0, 'requestCharge': 1.0, 'Status': 0, 'startTime': -1,
-                                           'endTime': -1, 'chargeCost': 0, 'serveCost': 0, 'charged': 0, 'NO': 'F1',
-                                           'servingPile': -1, 'otherinfo': ''}])
+def chargereq(name,chargemode,chargeamount):
+    if(waiting_queue.full()):#队列满了
+        datasend(['__SubmitRequestReturn', 0, {'Billid': 1, 'USERID': '1', 'CreateTime':
+1684540800, 'chargeMode': 0, 'requestCharge': 1.0, 'Status': 0, 'startTime': -1,
+'endTime': -1, 'chargeCost': 0, 'serveCost': 0, 'charged': 0, 'NO': 'F1',
+'servingPile': -1, 'otherinfo': ''}])
+        return
+    else:
+        global billid
+        global time
+        global f
+        global s
+        no=''
+        if chargemode==0:#快充
+            f=f+1
+            no=f'F{f}'
+        else:
+            s=s+1
+            no=f'S{s}'
+        billid=billid+1
+        waiting_queue.put({'Billid': billid, 'USERID': name, 'CreateTime':
+time, 'chargeMode': chargemode, 'requestCharge': chargeamount, 'Status': 0, 'startTime': -1,
+'endTime': -1, 'chargeCost': 0, 'serveCost': 0, 'charged': 0, 'NO': no,
+'servingPile': -1, 'otherinfo': ''})
+
+def rmcharging(info):
+    global time
+    chargemode=info['chargeMode']
+    chargeamount=info['requestCharge']-(time-info['startTime'])*(chargemode%1*43+7)
+    if(waiting_queue.full()):#队列满了
+        datasend(['__SubmitRequestReturn', 0, {'Billid': 1, 'USERID': '1', 'CreateTime':
+1684540800, 'chargeMode': 0, 'requestCharge': 1.0, 'Status': 0, 'startTime': -1,
+'endTime': -1, 'chargeCost': 0, 'serveCost': 0, 'charged': 0, 'NO': 'F1',
+'servingPile': -1, 'otherinfo': ''}])
+        return
+    else:
+        global billid
+        global f
+        global s
+        no=''
+        if chargemode==0:#快充
+            f=f+1
+            no=f'F{f}'
+        else:
+            s=s+1
+            no=f'S{s}'
+        billid=billid+1
+        waiting_queue.put({'Billid': billid, 'USERID': name, 'CreateTime':
+time, 'chargeMode': chargemode, 'requestCharge': chargeamount, 'Status': 0, 'startTime': -1,
+'endTime': -1, 'chargeCost': 0, 'serveCost': 0, 'charged': 0, 'NO': no,
+'servingPile': -1, 'otherinfo': ''})
+def changemode(name):
+    for waiting in waiting_queue:
+        if waiting['USERID']==name:#在等待队列，直接改变
+            global f
+            global s
+            if waiting['chargeMode']==0:
+                f=f-1
+                s=s+1
+                no = f'S{s}'
+                waiting['chargeMode']=1
+                waiting['NO']=no
+            else:
+                f=f+1
+                s=s-1
+                no = f'F{f}'
+                waiting['chargeMode']=0
+                waiting['NO']=no
+            return
+    #没找到，已经在充电区
+    rmcharging()
+
+
 
 
 def datasend(data):
@@ -107,6 +194,17 @@ allCost float,
 other vchar(40),
 PRIMARY KEY(DetailedBillNum,USER_ID,seq)
 )''')
+
+
+def timer():
+    timerr = threading.Timer(5, timer)
+    timerr.start()
+    global time
+    time = time + 1
+    print(time)
+
+
+timer()
 
 while True:
     try:
